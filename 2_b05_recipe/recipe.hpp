8f915a1f31a80b7a9f6a12be1fd4c2e048b1ea76
Yehor <yehor.kryklii99@gmail.com>
2018-10-26 21:30:18 +0300

    create constructor of Recipe, privat variables and inline methods

--- 2_b05_recipe/recipe.hpp
+++ 2_b05_recipe/recipe.hpp
@@ -18,16 +18,35 @@ class Recipe
 public:
 
 /*-----------------------------------------------------------------*/
+	const std::string & getName() const;
 
-	// TODO: public members
+	const std::string & getDescription() const;
+
+	const std::string & getAuthor() const;
+
+	int getIngredientsCount() const;
+
+
+
+	Recipe(
+		const std::string & _name
+		, const std::string & _description
+		, const std::string & _author
+	);
 
 /*-----------------------------------------------------------------*/
 
 private:
 
+	std::string nameOfRecipe;
+
+	std::string descriptionOfRecipe;
+
+	std::string authorOfRecipe;
+
 /*-----------------------------------------------------------------*/
 
-	// TODO: private members
+	
 
 /*-----------------------------------------------------------------*/
 
@@ -35,7 +54,20 @@ private:
 
 /*****************************************************************************/
 
-// TODO: inline functions
+inline const std::string & Recipe::getName() const
+{
+	return nameOfRecipe;
+}
+
+inline const std::string & Recipe::getDescription() const
+{
+	return descriptionOfRecipe;
+}
+
+inline const std::string & Recipe::getAuthor() const
+{
+	return authorOfRecipe;
+}
 
 /*****************************************************************************/
 
